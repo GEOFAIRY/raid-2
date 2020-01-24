ab75dfe3490e5c04769abae4416a13e64adb4316
Kyran <krs19@xtra.co.nz>
2020-01-24 18:16:09 +1300

    Removed restruct files

--- raid-api/app/__init__.py
+++ raid-api/app/__init__.py
@@ -1,34 +1,34 @@
 import os
 
-from flask import Flask, jsonify, request
+from flask import Flask
 from flask_cors import CORS
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 from flask_httpauth import HTTPBasicAuth
+import logging
 
 auth = HTTPBasicAuth()
 
-#init the rest api
+# init the rest api
 app = Flask(__name__)
 CORS(app)
 
-#check the database exists
+# check the database exists
 baseDir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(baseDir, 'db.sqlite')
-app.config.from_pyfile(os.path.join(baseDir, 'config.cfg'), silent=True)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(baseDir, 'app/resource/database/db.sqlite')
+app.config.from_pyfile(os.path.join(baseDir, 'app/resource/config.cfg'), silent=True)
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
-
-import logging
-
 # logging.basicConfig()
 # logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
-#init endpoints
-from app import raids
-from app import users
-from app import parties
-
+# init endpoints
+from app.main.view import party_view
+from app.main.view import game_view
+from app.main.view import party_user_view
+from app.main.view import phase_view
+from app.main.view import raid_view
+from app.main.view import user_view
--- raid-api/app/helpers/emails.py
+++
@@ -1,4 +0,0 @@
-from validate_email import validate_email
-
-def emailValid(email):
-    return validate_email(email)
